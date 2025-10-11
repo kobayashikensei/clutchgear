9ada1be13ffcf8efbcb06fe3215dc75bff0bfcce
kobayashikensei <ken4342@icloud.com>
2025-10-11 12:39:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -599,7 +599,7 @@ const PLAYERS = [
   {
     id: "pl-tenz",
     name: "Tenz",
-    team: "",
+    team: "Unattached",
     devices: [
       { role: "Keyboard", itemId: "p-Wooting-80HE" },
       { role: "Mouse", itemId: "p-pulsar-TenZ-Edition" },
